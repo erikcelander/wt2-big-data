479208f67895e165ecce7eaf03f20a710fe56a1e
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-05-08 14:52:52 +0200

    fetch all players from top 5 leagues with min 20 apps this season

--- data_gathering/data.py
+++ data_gathering/data.py
@@ -1,22 +1,32 @@
 import asyncio
-import json
-import ssl
-
-import aiohttp
 import certifi
-
+import ssl
+import aiohttp
 from understat import Understat
+import json
 
-async def main():    
-  
+
+async def main():
     ssl_context = ssl.create_default_context(cafile=certifi.where())
 
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
         understat = Understat(session)
+        leagues = ["epl", "la_liga", "ligue_1", "bundesliga", "serie_a"]
+        min_games_played = 20
 
-        # Fetch player data for a specific league (e.g., English Premier League)
-        epl = await understat.get_league_players("epl", 2022)
-        print(json.dumps(epl, indent=4, sort_keys=True))
+        filtered_players = {}
+
+        for league in leagues:
+            players = await understat.get_league_players(league, 2022)
+            filtered_players[league] = [player for player in players if int(player["games"]) >= min_games_played]
+
+        # Print the total number of players left after filtering
+        total_players = sum(len(players) for players in filtered_players.values())
+        print(f"Total players with 10 or more games played: {total_players}")
+
+        # print(json.dumps(filtered_players, indent=4, sort_keys=True))
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
